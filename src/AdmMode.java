ca9ee0993885295a23a207d36ad63865073d0159
Estudiante <Estudiante@FI454402-34.unal.loc>
2016-10-13 15:56:55 -0500

    Cientifico ejercicio herencia

---
+++ src/AdmMode.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class AdmMode extends Mode{
+    public void work(){System.out.println("administrate");}
+    public void drink(){System.out.println("tea");}    
+}
---
+++ src/WriteMode.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class WriteMode extends Mode{
+    public void work(){System.out.println("write");}
+    public void drink(){System.out.println("coffee");}
+}
